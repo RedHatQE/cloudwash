d527cdf9c089e307c8775391a9ff403f73624abd
jyejare <jyejare@redhat.com>
2022-09-17 00:17:02 +0530

    Making settings more precise

--- cloudwash/config.py
+++ cloudwash/config.py
@@ -4,13 +4,20 @@ from dynaconf import Validator
 from cloudwash.logger import logger
 
 # Initialize and Configure Settings
-settings = Dynaconf(envvar_prefix="CLEANUP", settings_file="settings.yaml")
+settings = Dynaconf(
+    core_loaders=["YAML"],
+    envvar_prefix="CLEANUP",
+    settings_file="settings.yaml",
+    preload=["conf/*.yaml"],
+    envless_mode=True,
+    lowercase_read=True,
+)
 
 
 def validate_provider(provider_name):
     provider = provider_name.upper()
     provider_settings = [
-        f"{provider}.{setting_key}" for setting_key in settings.to_dict()["PROVIDERS"].get(provider)
+        f"{provider}.{setting_key}" for setting_key in settings.to_dict().get(provider)
     ]
     settings.validators.register(Validator(*provider_settings, ne=None))
     try:
--- cloudwash/providers/aws.py
+++ cloudwash/providers/aws.py
@@ -11,8 +11,8 @@ def cleanup(**kwargs):
 
     is_dry_run = kwargs["dry_run"]
     data = ['VMS', 'DISCS', 'PIPS', 'RESOURCES']
-    regions = settings.providers.ec2.regions
-    with compute_client("ec2", ec2_region="us-west-2") as client:
+    regions = settings.aws.auth.regions
+    with compute_client("aws", aws_region="us-west-2") as client:
         if "all" in regions:
             regions = client.list_regions()
     for region in regions:
@@ -20,43 +20,52 @@ def cleanup(**kwargs):
         dry_data['VMS']['skip'] = []
         for items in data:
             dry_data[items]['delete'] = []
-        with compute_client("ec2", ec2_region=region) as ec2_client:
+        with compute_client("aws", aws_region=region) as aws_client:
             # Dry Data Collection Defs
             def dry_vms():
-                all_vms = ec2_client.list_vms()
+                all_vms = aws_client.list_vms()
                 for vm in all_vms:
-                    if vm.name in settings.providers.ec2.except_vm_list:
+                    if vm.name in settings.aws.exception.vm.vm_list:
                         dry_data["VMS"]["skip"].append(vm.name)
                         continue
-                    elif total_running_time(vm).minutes >= settings.sla_minutes:
-                        if vm.name in settings.providers.ec2.except_vm_stop_list:
+                    elif total_running_time(vm).minutes >= settings.aws.criteria.vm.sla_minutes:
+                        if vm.name in settings.aws.exception.vm.stop_list:
                             dry_data["VMS"]["stop"].append(vm.name)
                             continue
-                        elif vm.name.startswith(settings.delete_vm):
+                        elif vm.name.startswith(settings.aws.criteria.vm.delete_vm):
                             dry_data["VMS"]["delete"].append(vm.name)
                 return dry_data["VMS"]
 
             def dry_nics():
-                rnics = ec2_client.get_all_unused_network_interfaces()
-                [dry_data["NICS"]["delete"].append(dnic["NetworkInterfaceId"]) for dnic in rnics]
-                return dry_data["NICS"]["delete"]
+                rnics = []
+                if settings.aws.criteria.nic.unassigned:
+                    rnics = aws_client.get_all_unused_network_interfaces()
+                    [
+                        dry_data["NICS"]["delete"].append(dnic["NetworkInterfaceId"])
+                        for dnic in rnics
+                    ]
+                return rnics
 
             def dry_discs():
-                rdiscs = ec2_client.get_all_unattached_volumes()
-                [dry_data["DISCS"]["delete"].append(ddisc["VolumeId"]) for ddisc in rdiscs]
-                return dry_data["DISCS"]["delete"]
+                rdiscs = []
+                if settings.aws.criteria.disc.unassigned:
+                    rdiscs = aws_client.get_all_unattached_volumes()
+                    [dry_data["DISCS"]["delete"].append(ddisc["VolumeId"]) for ddisc in rdiscs]
+                return rdiscs
 
             def dry_pips():
-                rpips = ec2_client.get_all_disassociated_addresses()
-                [dry_data["PIPS"]["delete"].append(dpip["AllocationId"]) for dpip in rpips]
-                return dry_data["PIPS"]["delete"]
+                rpips = []
+                if settings.aws.criteria.public_ip.unassigned:
+                    rpips = aws_client.get_all_disassociated_addresses()
+                    [dry_data["PIPS"]["delete"].append(dpip["AllocationId"]) for dpip in rpips]
+                return rpips
 
             # Remove / Stop VMs
             def remove_vms(avms):
                 # Remove VMs
-                [ec2_client.get_vm(vm_name).delete() for vm_name in avms["delete"]]
+                [aws_client.get_vm(vm_name).delete() for vm_name in avms["delete"]]
                 # Stop VMs
-                [ec2_client.get_vm(vm_name).stop() for vm_name in avms["stop"]]
+                [aws_client.get_vm(vm_name).stop() for vm_name in avms["stop"]]
 
             # Actual Cleaning and dry execution
             logger.info(f"\nResources from the region: {region}")
@@ -69,18 +78,18 @@ def cleanup(**kwargs):
                     logger.info(f"Skipped VMs: \n{avms['skip']}")
             if kwargs["nics"] or kwargs["_all"]:
                 rnics = dry_nics()
-                if not is_dry_run:
-                    ec2_client.remove_all_unused_nics()
+                if not is_dry_run and rnics:
+                    aws_client.remove_all_unused_nics()
                     logger.info(f"Removed NICs: \n{rnics}")
             if kwargs["discs"] or kwargs["_all"]:
                 rdiscs = dry_discs()
-                if not is_dry_run:
-                    ec2_client.remove_all_unused_volumes()
+                if not is_dry_run and rdiscs:
+                    aws_client.remove_all_unused_volumes()
                     logger.info(f"Removed Discs: \n{rdiscs}")
             if kwargs["pips"] or kwargs["_all"]:
                 rpips = dry_pips()
-                if not is_dry_run:
-                    ec2_client.remove_all_unused_ips()
+                if not is_dry_run and rpips:
+                    aws_client.remove_all_unused_ips()
                     logger.info(f"Removed PIPs: \n{rpips}")
             if is_dry_run:
                 echo_dry(dry_data)
